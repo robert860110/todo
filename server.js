937644c29d0c8983afb9193085d4c68dbaf8d1b7
Guanqun Bao <guanqun.bao@gmail.com>
2016-05-11 09:04:38 -0700

    add underscore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParse = require('body-parser');
 var app = express();
+var _ = require('underscore');
 var PORT = process.env.PORT || 3000;
 
 var todos = [];
@@ -20,24 +21,35 @@ app.get('/todos', function(req, res) {
 })
 
 app.post('/todos', function(req, res) {
-    var body = req.body;
-    var todo = body;
-    todo.id = todoNextId;
-    todos.push(todo);
-    todoNextId++;
+    var body = _.pick(req.body, 'description', 'complete');
 
-    res.json(todos);
+
+    if (!_.isBoolean(body.complete) || _.isString(body.description) || body.description.trim().length === 0)
+
+    {
+        body.id = todoNextId;
+        body.description = body.description.trim();
+
+        todos.push(body);
+        todoNextId++;
+
+        res.json(todos);
+    }
+    else res.status(404).sned();
 })
 
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id);
-    var matchedTodo;
-    todos.forEach(function(todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
+
+    var matchedTodo = _.findWhere(todos, { id: todoId });
+
+    // var matchedTodo;
+    // todos.forEach(function(todo) {
+    //     if (todoId === todo.id) {
+    //         matchedTodo = todo;
+    //     }
+    // });
 
     if (matchedTodo) {
         res.json(matchedTodo);
