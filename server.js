362160cc5d410fc40226d6584b9a30dd7bbed94b
Guanqun Bao <guanqun.bao@gmail.com>
2016-05-11 17:13:56 -0700

    add PUT

--- server.js
+++ server.js
@@ -34,8 +34,7 @@ app.post('/todos', function(req, res) {
         todoNextId++;
 
         res.json(todos);
-    }
-    else res.status(404).sned();
+    } else res.status(404).sned();
 })
 
 // GET /todos/:id
@@ -58,5 +57,52 @@ app.get('/todos/:id', function(req, res) {
     }
 
 })
+
+// DELETE /todos/:id
+
+// GET /todos/:id
+app.delete('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id);
+    var matchedTodo = _.findWhere(todos, { id: todoId });
+
+    if (!matchedTodo) {
+        res.status(404).send('no todo data found with that id');
+
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(todos);
+    }
+
+})
+
+// PUT /todos/:id
+
+app.put('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, { id: todoId });
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+
+})
+
 
 app.listen(PORT, function() { console.log('Express listening on port 3000'); });
