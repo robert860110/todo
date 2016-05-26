8122d24179a60da71aa0c2736acb142f44b00bfd
Guanqun Bao <guanqun.bao@gmail.com>
2016-05-26 14:58:36 -0700

    add login

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -124,6 +125,22 @@ app.post('/users', function(req, res) {
         res.status(400).json(e);
     });
 });
+
+
+app.post('/users/login', function(req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+    if (typeof body.email !== 'string' || typeof body.password !== 'string')
+        return res.status(400).send();
+    db.user.findOne({ where: { email: body.email } }).then(function(user) {
+        if (!user || !bcrypt.compareSync(body.password, user.password_hash)) {
+            return res.status(401).send();
+        }
+        res.json(user.toPublicJSON());
+    }, function(e) {
+        res.status(400).send();
+    })
+});
+
 
 db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
