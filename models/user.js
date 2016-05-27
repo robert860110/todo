d5686ed269d7b4d069f09c57d374ed408f5f0c52
Guanqun Bao <guanqun.bao@gmail.com>
2016-05-26 22:03:00 -0700

    auth token

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DateTypes) {
-    return sequelize.define('user', {
+    var user = sequelize.define('user', {
         email: {
             type: DateTypes.STRING,
             allowNull: false,
@@ -41,11 +43,46 @@ module.exports = function(sequelize, DateTypes) {
             }
 
         },
+        classMethods: {
+            authenticate: function(body) {
+                return new Promise(function(resolve, reject) {
+
+                    if (typeof body.email !== 'string' || typeof body.password !== 'string')
+                        return reject();
+                    user.findOne({ where: { email: body.email } }).then(function(user) {
+                            if (!user || !bcrypt.compareSync(body.password, user.password_hash)) {
+                                return reject();
+                            }
+                            return resolve(user);
+                        },
+                        function(e) {
+                            return reject();
+                        })
+                });
+            }
+        },
         instanceMethods: {
             toPublicJSON: function() {
                 var json = this.toJSON();
                 return _.pick(json, 'email', 'createdAt', 'updatedAt');
+            },
+            generateToken: function(type) {
+                if (typeof type !== 'string')
+                    return undefined;
+                try {
+                    var stringData = JSON.stringify({ id: this.get('id'), type: type });
+                    var encryptedData = cryptojs.AES.encrypt(stringData, 'abc1231@!').toString();
+                    var token = jwt.sign({
+                        token: encryptedData,
+                        iat: Math.floor(Date.now() / 1000) - 30,
+                        exp: Math.floor(Date.now() / 1000) + 3600,
+                    }, 'shhhhh');
+                    return token;
+                } catch (e) {
+                    return undefined;
+                }
             }
         }
     });
+    return user;
 }
--- server.js
+++ server.js
@@ -129,16 +129,12 @@ app.post('/users', function(req, res) {
 
 app.post('/users/login', function(req, res) {
     var body = _.pick(req.body, 'email', 'password');
-    if (typeof body.email !== 'string' || typeof body.password !== 'string')
-        return res.status(400).send();
-    db.user.findOne({ where: { email: body.email } }).then(function(user) {
-        if (!user || !bcrypt.compareSync(body.password, user.password_hash)) {
-            return res.status(401).send();
-        }
-        res.json(user.toPublicJSON());
-    }, function(e) {
-        res.status(400).send();
-    })
+
+    db.user.authenticate(body).then(function(user) {
+        return res.header('Auth', user.generateToken('authentication')).json(user.toPublicJSON());
+    }, function(error) {
+        return res.status(401).send();
+    });
 });
 
 
